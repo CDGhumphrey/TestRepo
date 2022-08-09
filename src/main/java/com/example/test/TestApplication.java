24ce81cb530bbf0abaea445a39c46156fa30c9ae
dsa <s@gmail.com>
2022-08-09 08:45:29 -0400

    Added a seconf print line

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,6 +8,7 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		System.out.println("second");
+		System.out.println("first second");
 		SpringApplication.run(TestApplication.class, args);
 	}
 
